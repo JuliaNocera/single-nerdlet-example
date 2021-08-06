6651247e577cea8af98f5c4f6f36c037ac75de80
Jewels Nocera <jnocera@newrelic.com>
2021-08-06 16:26:01 -0700

    chore: clean up header and nav

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-import {Dropdown, DropdownItem, NerdletStateContext, nerdlet} from 'nr1'
+import {BlockText, Dropdown, DropdownItem, NerdletStateContext, nerdlet, Spacing} from 'nr1'
 
 const Header = () => {
   const handleSelect = (value) => {
-    console.log(value)
     nerdlet.setUrlState({
       iceCream: value
     })
@@ -20,27 +19,36 @@ const Header = () => {
 
   return (
     <NerdletStateContext.Consumer>
-      {(nerdletState) => console.log({nerdletState}) || (
-        <Dropdown title={`What type of ice cream do you like? ${nerdletState && nerdletState.iceCream ? nerdletState.iceCream : ''} `}>
-          <DropdownItem
-            selected={isSelected(nerdletState, 'chocolate')}
-            onClick={() => handleSelect('chocolate')}
-          >
-            Chocolate
+      {(nerdletState) => (
+        <div style={{display: 'flex', alignItems: 'center'}}>
+          <Dropdown title={`What type of ice cream do you like? ${nerdletState && nerdletState.iceCream ? nerdletState.iceCream : ''} `}>
+            <DropdownItem
+              selected={isSelected(nerdletState, 'chocolate')}
+              onClick={() => handleSelect('chocolate')}
+            >
+              Chocolate
       </DropdownItem>
-          <DropdownItem
-            selected={isSelected(nerdletState, 'chocolate')}
-            onClick={() => handleSelect('strawberry')}>Strawberry</DropdownItem>
-          <DropdownItem
-            selected={isSelected(nerdletState, 'chocolate')}
-            onClick={() => handleSelect('vanilla')}>Vanilla</DropdownItem>
-          <DropdownItem
-            selected={isSelected(nerdletState, 'chocolate')}
-            onClick={() => handleSelect('none')}>Ice cream? No thank you.</DropdownItem>
-        </Dropdown>
+            <DropdownItem
+              selected={isSelected(nerdletState, 'chocolate')}
+              onClick={() => handleSelect('strawberry')}>Strawberry</DropdownItem>
+            <DropdownItem
+              selected={isSelected(nerdletState, 'chocolate')}
+              onClick={() => handleSelect('vanilla')}>Vanilla</DropdownItem>
+            <DropdownItem
+              selected={isSelected(nerdletState, 'chocolate')}
+              onClick={() => handleSelect('No thanks')}>Ice cream? No thank you.</DropdownItem>
+          </Dropdown>
+          { (nerdletState && nerdletState.iceCream) &&
+            <Spacing type={[Spacing.TYPE.MEDIUM]} >
+              <BlockText><em>Previously selected:</em> {nerdletState.iceCream}</BlockText>
+            </Spacing>
+          }
+        </div>
       )}
     </NerdletStateContext.Consumer>
   )
 }
 
 export default Header;
+
+
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -13,81 +13,118 @@ export default class NavBar extends React.Component {
     const isCurrentView = nerdletState.currentView === value
     return isCurrentView
   }
+
+  buildNerdletForNavigation = (stateToPass) => {
+
+    const {iceCream, currentView} = stateToPass
+    let nerdletForNavigation = {}
+
+    switch (currentView) {
+      case 'ONE':
+        nerdletForNavigation = {
+          id: `${MULTIPLE_NERDLETS_EXAMPLE_NERDPACK_ID}.home`,
+          urlState: {
+            iceCream,
+            currentView
+          }
+        }
+        return nerdletForNavigation
+      case 'TWO':
+        nerdletForNavigation = {
+          id: `${MULTIPLE_NERDLETS_EXAMPLE_NERDPACK_ID}.second`,
+          urlState: {
+            iceCream,
+            currentView
+          }
+        }
+        return nerdletForNavigation
+      case 'THREE':
+        nerdletForNavigation = {
+          id: `${MULTIPLE_NERDLETS_EXAMPLE_NERDPACK_ID}.third`,
+          urlState: {
+            iceCream,
+            currentView
+          }
+        }
+        return nerdletForNavigation
+      case 'FOUR':
+        nerdletForNavigation = {
+          id: `${SINGLE_NERDLET_EXAMPLE_NERDPACK_ID}.home`,
+          urlState: {
+            iceCream,
+            currentView
+          }
+        }
+        return nerdletForNavigation
+    }
+
+    return nerdletForNavigation
+  }
+
+
+
   render() {
     return (
       <NerdletStateContext.Consumer>
-        {(nerdletState) => (
-          <Card>
-            <CardBody>
-              <Grid
-                gapType={Grid.GAP_TYPE.SMALL}
-              >
-                <GridItem columnSpan={3}>
-                  <Button
-                    type={this.isCurrentView(nerdletState, "ONE") ? Button.TYPE.PRIMARY : Button.TYPE.OUTLINE}
-                    style={{width: '100%', height: '100%'}}
-                    onClick={() => {
-                      // See docs for example: https://developer.newrelic.com/apis/navigation/
-                      const nerdlet = {
-                        id: `${MULTIPLE_NERDLETS_EXAMPLE_NERDPACK_ID}.home`,
-                        urlState: {
-                          currentView: 'ONE'
-                        }
-                      }
-                      navigation.replaceNerdlet(nerdlet)
-                    }}
-                  >One</Button>
-                </GridItem>
-                <GridItem columnSpan={3}>
-                  <Button
-                    type={this.isCurrentView(nerdletState, "TWO") ? Button.TYPE.PRIMARY : Button.TYPE.OUTLINE}
-                    style={{width: '100%', height: '100%'}}
-                    onClick={() => {
-                      // See docs for example: https://developer.newrelic.com/apis/navigation/
-                      const nerdlet = {
-                        id: `${MULTIPLE_NERDLETS_EXAMPLE_NERDPACK_ID}.second`,
-                        urlState: {
-                          currentView: 'TWO'
-                        }
-                      }
-                      navigation.replaceNerdlet(nerdlet)
-                    }}
-                  >Two</Button>
-                </GridItem>
-                <GridItem columnSpan={3}>
-                  <Button
-                    type={this.isCurrentView(nerdletState, "THREE") ? Button.TYPE.PRIMARY : Button.TYPE.OUTLINE}
-                    style={{width: '100%', height: '100%'}}
-                    onClick={() => {
-                      const nerdlet = {
-                        id: `${MULTIPLE_NERDLETS_EXAMPLE_NERDPACK_ID}.third`,
-                        urlState: {
-                          currentView: 'THREE'
-                        }
-                      }
-                      navigation.replaceNerdlet(nerdlet)
-                    }}
-                  >Three</Button>
-                </GridItem>
-                <GridItem columnSpan={3}>
-                  <Button
-                    type={this.isCurrentView(nerdletState, "FOUR") ? Button.TYPE.PRIMARY : Button.TYPE.OUTLINE}
-                    style={{width: '100%', height: '100%'}}
-                    onClick={() => {
-                      const nerdlet = {
-                        id: `${SINGLE_NERDLET_EXAMPLE_NERDPACK_ID}.home`,
-                        urlState: {
-                          currentView: 'FOUR'
-                        }
-                      }
-                      navigation.replaceNerdlet(nerdlet)
-                    }}
-                  >Four</Button>
-                </GridItem>
-              </Grid>
-            </CardBody>
-          </Card>
-        )}
+        {(nerdletState) => {
+          // State from elsewhere that I want to persist when the current nerdlet is changed
+          const iceCreamState = nerdletState && nerdletState.iceCream
+            ? nerdletState.iceCream
+            : undefined
+
+          return (
+            <Card>
+              <CardBody>
+                <Grid
+                  gapType={Grid.GAP_TYPE.SMALL}
+                >
+                  <GridItem columnSpan={3}>
+                    <Button
+                      type={this.isCurrentView(nerdletState, "ONE") ? Button.TYPE.PRIMARY : Button.TYPE.OUTLINE}
+                      style={{width: '100%', height: '100%'}}
+                      onClick={() => {
+                        // See docs for example: https://developer.newrelic.com/apis/navigation/
+                        const nerdlet = this.buildNerdletForNavigation({iceCream: iceCreamState, currentView: "ONE"})
+                        navigation.replaceNerdlet(nerdlet)
+                      }}
+                    >One</Button>
+                  </GridItem>
+                  <GridItem columnSpan={3}>
+                    <Button
+                      type={this.isCurrentView(nerdletState, "TWO") ? Button.TYPE.PRIMARY : Button.TYPE.OUTLINE}
+                      style={{width: '100%', height: '100%'}}
+                      onClick={() => {
+                        // See docs for example: https://developer.newrelic.com/apis/navigation/
+                        const nerdlet = this.buildNerdletForNavigation({iceCream: iceCreamState, currentView: "TWO"})
+                        navigation.replaceNerdlet(nerdlet)
+                      }}
+                    >Two</Button>
+                  </GridItem>
+                  <GridItem columnSpan={3}>
+                    <Button
+                      type={this.isCurrentView(nerdletState, "THREE") ? Button.TYPE.PRIMARY : Button.TYPE.OUTLINE}
+                      style={{width: '100%', height: '100%'}}
+                      onClick={() => {
+                        const nerdlet = this.buildNerdletForNavigation({iceCream: iceCreamState, currentView: "THREE"})
+                        navigation.replaceNerdlet(nerdlet)
+                      }}
+                    >Three</Button>
+                  </GridItem>
+                  <GridItem columnSpan={3}>
+                    <Button
+                      type={this.isCurrentView(nerdletState, "FOUR") ? Button.TYPE.PRIMARY : Button.TYPE.OUTLINE}
+                      style={{width: '100%', height: '100%'}}
+                      onClick={() => {
+                        const nerdlet = this.buildNerdletForNavigation({iceCream: iceCreamState, currentView: "FOUR"})
+                        navigation.replaceNerdlet(nerdlet)
+                      }}
+                    >Four</Button>
+                  </GridItem>
+                </Grid>
+              </CardBody>
+            </Card>
+          )
+        }}
       </NerdletStateContext.Consumer>
     )
   }
